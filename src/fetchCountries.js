31e3af8c4a40b5857c9e3f49f092e844664d1b46
Rafał Biżek <rafal.bizek@gmail.com>
2023-05-14 08:41:22 +0200

    update/fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
-const baseUrl = 'https://restcountries.com/v3.1/name/{name}';
+const baseUrl = 'https://restcountries.com/v3.1/';
 
 export const fetchCountries = name => {
   const fields = 'fields=name.official,capital,population,flags.svg,languages';
-  const url = `${baseUrl}name/${name}?${fields}`;
+  const url = `${baseUrl}name/${encodeURIComponent(name)}?${fields}`;
 
   return fetch(url)
     .then(response => {
@@ -12,15 +12,26 @@ export const fetchCountries = name => {
       return response.json();
     })
     .then(data => {
+      if (!Array.isArray(data)) {
+        return []; // Zwraca pustą tablicę w przypadku braku wyników
+      }
+
       const countries = data.map(country => {
+        const commonName =
+          country.name && country.name.official
+            ? country.name.official
+            : 'unknown';
+        const flag =
+          country.flags && country.flags.svg ? country.flags.svg : 'unknown';
         return {
-          name: country.name.common,
-          flag: country.flags.svg,
+          name: commonName,
+          flag: flag,
           capital: country.capital?.[0] || 'unknown',
           population: country.population || 'unknown',
           languages: Object.values(country.languages).join(', ') || 'unknown',
         };
       });
+
       return countries;
     })
     .catch(error => {
